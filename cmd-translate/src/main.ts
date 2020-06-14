91879d69f37443a3cfc335c8a535f85261639d25
ergouwang <mrergouwang@gmail.com>
2020-06-15 00:39:25 +0800

    ‘翻译功能完成'

--- cmd-translate/src/main.ts
+++ cmd-translate/src/main.ts
@@ -1,11 +1,11 @@
 import * as querystring from "querystring";
 import md5 = require("md5");
+import {appId, appSecret} from "./private";
+import {checkServerIdentity} from "tls";
 const https = require('https');
 
 export  const translate = (word)=>{
 
-    const appId = ''
-    const appSecret = ''
     const salt = Math.random()
     const sign = md5(appId+word+salt+appSecret)
 
@@ -19,17 +19,43 @@ export  const translate = (word)=>{
     })
 
     const options = {
-        hostname: 'api.fanyi.baidu.com',
+        hostname: 'fanyi-api.baidu.com',
         port: 443,
         path: '/api/trans/vip/translate?'+query,
         method: 'GET'
     };
 
     const req = https.request(options, (res) => {
+        let chunks = []
 
-        res.on('data', (d) => {
-            process.stdout.write(d);
+        res.on('data', (chunk) => {
+            chunks.push(chunk)
         });
+
+        res.on('end',()=>{
+            const string = Buffer.concat(chunks).toString()
+
+            type BaiduResult = {
+                error_code?:string,
+                error_msg?:string,
+                from:string,
+                to:string,
+                trans_result:{
+                    src:string,
+                    dst:string
+                }[]
+            }
+
+            const  object:BaiduResult = JSON.parse(string)
+            if (object.error_code){
+                console.log(object.error_msg);
+                process.exit(2)
+            }else {
+                console.log(object.trans_result[0].dst);
+                process.exit(0)
+            }
+
+        })
     });
 
     req.on('error', (e) => {
---
+++ cmd-translate/src/private.ts
@@ -0,0 +1,2 @@
+export const appId = '20200614000495476'
+export const appSecret = 'kajRMpPEwgPQR19HZN1L'
