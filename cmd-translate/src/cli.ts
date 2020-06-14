669da87b1a59b843619ba3fe03b8f9d11c690bc6
ergouwang <mrergouwang@gmail.com>
2020-06-14 21:17:08 +0800

    '成功接收命令行输入单词'

--- cmd-translate/src/cli.ts
+++ cmd-translate/src/cli.ts
@@ -5,7 +5,12 @@ const program = new commander.Command();
 program.version('0.0.1')
     .name('fy')
     // 尖括号表示这个是必选的
-    .usage('<english>')
+    .usage('<English>')
+    .arguments('<English>')
+    .action(function (english) {
+        console.log('english',english);
+        // translate(english)
+    })
 
 program.parse(process.argv)
 
