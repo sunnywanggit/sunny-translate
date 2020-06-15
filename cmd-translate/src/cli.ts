68f9efdfec4a4f2dd90e18cfe4cc7b9d3b64e512
ergouwang <mrergouwang@gmail.com>
2020-06-15 14:33:34 +0800

    使用表驱动编程优化代码

--- cmd-translate/src/cli.ts
+++ cmd-translate/src/cli.ts
@@ -1,18 +1,18 @@
 import * as commander from "commander";
 import {translate} from "./main";
+
 const program = new commander.Command();
 
 
-program.version('0.0.1')
+let command = program.version('0.0.1')
     .name('fy')
     // 尖括号表示这个是必选的
     .usage('<English>')
     .arguments('<English>')
     .action(function (english) {
         translate(english)
-    })
-
-program.parse(process.argv)
+    });
+program.parse(process.argv);
 
 
 
